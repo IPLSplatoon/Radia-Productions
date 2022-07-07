e9a0899f341c3d40d8975d649fe94151fc702e62
Restyled.io <commits@restyled.io>
2022-07-07 19:48:48 +0000

    Restyled by black

--- discordBot/bot/cogs/commentators.py
+++ discordBot/bot/cogs/commentators.py
@@ -32,26 +32,36 @@ class Commentators(commands.Cog):
         try:
             if twitter[0] == "@":  # If the user add @, remove it from string
                 twitter = twitter[1:]
-            elif "https://twitter.com/" in twitter:  # if they gave the full link, remove the link portion
+            elif (
+                "https://twitter.com/" in twitter
+            ):  # if they gave the full link, remove the link portion
                 twitter = twitter.replace("https://twitter.com/home", "")
             elif re.search(discord_mention, twitter):
-                await ctx.send(embed=utils.Embed(title="Set Commentator Twitter Handle Error!",
-                                                 description=f"You Twitter handle contains a Discord mention!\n"
-                                                             f"Try the command again without the `@` in your "
-                                                             f"Twitter handle"))
+                await ctx.send(
+                    embed=utils.Embed(
+                        title="Set Commentator Twitter Handle Error!",
+                        description=f"You Twitter handle contains a Discord mention!\n"
+                        f"Try the command again without the `@` in your "
+                        f"Twitter handle",
+                    )
+                )
                 return
-            response = await self.database.set_comms_info(str(ctx.message.author.id), name, twitter, pronouns)
+            response = await self.database.set_comms_info(
+                str(ctx.message.author.id), name, twitter, pronouns
+            )
             if response:
                 embed = utils.Embed(
-                    title=f"Set Commentator Profile", description="We've set your profile")
+                    title=f"Set Commentator Profile",
+                    description="We've set your profile",
+                )
                 embed.add_field(name="Name", value=f"{name}", inline=False)
-                embed.add_field(
-                    name="Twitter", value=f"@{twitter}", inline=False)
-                embed.add_field(name="Pronouns",
-                                value=f"{pronouns}", inline=False)
+                embed.add_field(name="Twitter", value=f"@{twitter}", inline=False)
+                embed.add_field(name="Pronouns", value=f"{pronouns}", inline=False)
                 await ctx.send(embed=embed)
         except Exception as err:
-            await ctx.send(embed=utils.Embed(title="Error", description=f"```\n{err}\n```"))
+            await ctx.send(
+                embed=utils.Embed(title="Error", description=f"```\n{err}\n```")
+            )
             pass
 
     @commentators.command()
@@ -70,20 +80,23 @@ class Commentators(commands.Cog):
             query = await self.database.get_comms_info(str(ctx.message.author.id))
             description = f"Your Commentator profile"
         if query:
-            embed = utils.Embed(title="Commentator Profile",
-                                description=f"{description}")
+            embed = utils.Embed(
+                title="Commentator Profile", description=f"{description}"
+            )
             embed.add_field(name="Name", value=f"{query.name}", inline=False)
-            embed.add_field(
-                name="Twitter", value=f"@{query.twitter}", inline=False)
-            embed.add_field(name="Pronouns",
-                            value=f"{query.pronouns}", inline=False)
+            embed.add_field(name="Twitter", value=f"@{query.twitter}", inline=False)
+            embed.add_field(name="Pronouns", value=f"{query.pronouns}", inline=False)
             embed.add_field(name="No Show", value=f"{query.no_show}")
             embed.add_field(name="No Alert", value=f"{query.no_alert}")
             await ctx.send(embed=embed)
             return
         else:
-            await ctx.send(embed=utils.Embed(title="No Profile Found",
-                                             description="Query has no commentator profile"))
+            await ctx.send(
+                embed=utils.Embed(
+                    title="No Profile Found",
+                    description="Query has no commentator profile",
+                )
+            )
             return
 
     @commentators.command(aliases=["setNoShow", "setnoshow"])
@@ -92,27 +105,39 @@ class Commentators(commands.Cog):
         Set the no show, to avoid showing on commentators details
         """
         if no_show.upper() in ["TRUE", "YES"]:
-            response = await self.database.set_comms_no_show(str(ctx.message.author.id), True)
+            response = await self.database.set_comms_no_show(
+                str(ctx.message.author.id), True
+            )
             try:
                 if response:
                     embed = utils.Embed(
-                        title="Commentator No Show", description=f"Set your no show state")
+                        title="Commentator No Show",
+                        description=f"Set your no show state",
+                    )
                     embed.add_field(name="No Show", value=f"True")
                     await ctx.send(embed=embed)
                     return
             except Exception as err:
-                await ctx.send(embed=utils.Embed(title="Error", description=f"```\n{err}\n```"))
+                await ctx.send(
+                    embed=utils.Embed(title="Error", description=f"```\n{err}\n```")
+                )
         elif no_show.upper() in ["FALSE", "NO"]:
-            response = await self.database.set_comms_no_show(str(ctx.message.author.id), False)
+            response = await self.database.set_comms_no_show(
+                str(ctx.message.author.id), False
+            )
             try:
                 if response:
                     embed = utils.Embed(
-                        title="Commentator No Show", description=f"Set your no show state")
+                        title="Commentator No Show",
+                        description=f"Set your no show state",
+                    )
                     embed.add_field(name="No Show", value=f"False")
                     await ctx.send(embed=embed)
                     return
             except Exception as err:
-                await ctx.send(embed=utils.Embed(title="Error", description=f"```\n{err}\n```"))
+                await ctx.send(
+                    embed=utils.Embed(title="Error", description=f"```\n{err}\n```")
+                )
                 pass
 
     @commentators.command(aliases=["setNoAlert", "setnoalert"])
@@ -121,27 +146,39 @@ class Commentators(commands.Cog):
         Set the no alert, to avoid showing system alerts
         """
         if no_alert.upper() in ["TRUE", "YES"]:
-            response = await self.database.set_comms_no_alert(str(ctx.message.author.id), True)
+            response = await self.database.set_comms_no_alert(
+                str(ctx.message.author.id), True
+            )
             try:
                 if response:
                     embed = utils.Embed(
-                        title="Commentator No Alert", description=f"Set your no alert state")
+                        title="Commentator No Alert",
+                        description=f"Set your no alert state",
+                    )
                     embed.add_field(name="No Alert", value=f"True")
                     await ctx.send(embed=embed)
                     return
             except Exception as err:
-                await ctx.send(embed=utils.Embed(title="Error", description=f"```\n{err}\n```"))
+                await ctx.send(
+                    embed=utils.Embed(title="Error", description=f"```\n{err}\n```")
+                )
         elif no_alert.upper() in ["FALSE", "NO"]:
-            response = await self.database.set_comms_no_alert(str(ctx.message.author.id), False)
+            response = await self.database.set_comms_no_alert(
+                str(ctx.message.author.id), False
+            )
             try:
                 if response:
                     embed = utils.Embed(
-                        title="Commentator No Alert", description=f"Set your no alert state")
+                        title="Commentator No Alert",
+                        description=f"Set your no alert state",
+                    )
                     embed.add_field(name="No Alert", value=f"False")
                     await ctx.send(embed=embed)
                     return
             except Exception as err:
-                await ctx.send(embed=utils.Embed(title="Error", description=f"```\n{err}\n```"))
+                await ctx.send(
+                    embed=utils.Embed(title="Error", description=f"```\n{err}\n```")
+                )
                 pass
 
 
